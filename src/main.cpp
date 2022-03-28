adad8200642ec46a7bedb1701046629de02b77d0
guillermo_gerard <guilloag@gmail.com>
2022-03-28 20:16:48 -0300

    basic configuration

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,10 @@
 #include <Arduino.h>
 
 void setup() {
- 
-  pinMode(1, OUTPUT);
-  // put your setup code here, to run once:
+  Serial.begin(115200);
+  pinMode(D1, INPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  yield();
+  
 }

af7cb991d21f7f051a4c7111c0c7017f01fdf5db
guillermo_gerard <guillermo.gerard@radventure.com>
2022-03-28 19:49:24 -0300

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+#include <Arduino.h>
+
+void setup() {
+ 
+  pinMode(1, OUTPUT);
+  // put your setup code here, to run once:
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  yield();
+}

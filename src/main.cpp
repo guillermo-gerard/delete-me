412a0948a309640b28b5b073091b83f0536a40b4
guillermo_gerard <guilloag@gmail.com>
2022-04-08 21:50:25 -0300

    boton con pulsacion simple con maquinade estados

--- src/main.cpp
+++ src/main.cpp
@@ -5,31 +5,49 @@ const uint8_t boton = D1;
 void setup()
 {
   Serial.begin(115200);
+  Serial.println("Bienvenido");
   pinMode(boton, INPUT);
 }
 
 unsigned long inicioAntirrebote;
-const int tiempoAntirrebote = 20;
-bool botonEnAntirrebote = false;
-bool estadoBoton = false;
+const int tiempoAntirrebote = 30;
+int estadoActual = 0;
+
+const int ESPERANDO_PULSACION = 0;
+const int ANTIRREBOTE_PULSACION = 1;
+const int ESPERANDO_SOLTACION = 2;
+const int ANTIRREBOTE_SOLTACION = 3;
 
 void loop()
 {
-  int botonPresionado = digitalRead(boton);
-
-  if (botonPresionado && !botonEnAntirrebote && !estadoBoton)
+  switch (estadoActual)
   {
-    inicioAntirrebote = millis();
-    botonEnAntirrebote = true;
-    Serial.println("boton presionado");
-    estadoBoton = true;
-  }
-  if (millis() - tiempoAntirrebote >= inicioAntirrebote + tiempoAntirrebote)
-  {
-    botonEnAntirrebote = false;
-  }
-  if (!botonPresionado && !botonEnAntirrebote && estadoBoton)
-  {
-    estadoBoton = false;
+  case ESPERANDO_PULSACION:
+    if (digitalRead(boton) == HIGH)
+    {
+      inicioAntirrebote = millis();
+      estadoActual = ANTIRREBOTE_PULSACION;
+    }
+    break;
+  case ANTIRREBOTE_PULSACION:
+    if (millis() - inicioAntirrebote >= tiempoAntirrebote)
+    {
+      estadoActual = ESPERANDO_SOLTACION;
+    }
+    break;
+  case ESPERANDO_SOLTACION:
+    if (digitalRead(boton) == LOW)
+    {
+      inicioAntirrebote = millis();
+      estadoActual = ANTIRREBOTE_SOLTACION;
+      Serial.println("Pulsado");
+    }
+    break;
+  case ANTIRREBOTE_SOLTACION:
+    if (millis() - inicioAntirrebote >= tiempoAntirrebote)
+    {
+      estadoActual = ESPERANDO_PULSACION;
+    }
+    break;
   }
 }

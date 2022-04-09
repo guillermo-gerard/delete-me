c57ce0c56491bca75fda83aa39bd7e5c6f7ef672
guillermo_gerard <guilloag@gmail.com>
2022-04-08 21:12:51 -0300

    forma fea

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,35 @@
 #include <Arduino.h>
 
-void setup() {
+const uint8_t boton = D1;
+
+void setup()
+{
   Serial.begin(115200);
-  pinMode(D1, INPUT);
+  pinMode(boton, INPUT);
 }
 
-void loop() {
-  
+unsigned long inicioAntirrebote;
+const int tiempoAntirrebote = 20;
+bool botonEnAntirrebote = false;
+bool estadoBoton = false;
+
+void loop()
+{
+  int botonPresionado = digitalRead(boton);
+
+  if (botonPresionado && !botonEnAntirrebote && !estadoBoton)
+  {
+    inicioAntirrebote = millis();
+    botonEnAntirrebote = true;
+    Serial.println("boton presionado");
+    estadoBoton = true;
+  }
+  if (millis() - tiempoAntirrebote >= inicioAntirrebote + tiempoAntirrebote)
+  {
+    botonEnAntirrebote = false;
+  }
+  if (!botonPresionado && !botonEnAntirrebote && estadoBoton)
+  {
+    estadoBoton = false;
+  }
 }
